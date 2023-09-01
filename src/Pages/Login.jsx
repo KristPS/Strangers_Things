81b857dedc3bcae199f2b7ded245d3d759133219
KristPS <126523107+KristPS@users.noreply.github.com>
2023-09-01 14:24:44 -0400

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,86 +1,77 @@
-import { useState } from "react";
-import AuthForm from "../Components/AuthForm";
-import { useNavigate } from "react-router-dom";
-import NavBar from "../Components/NavBar.jsx";
+import { useState, useContext } from 'react';
+import { Context } from '../ContextStore';
+import { loginUser } from '../services/userData'
+import { Form, Button, Spinner, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import SimpleSider from '../components/Siders/SimpleSider';
 
-const COHORT_NAME = "2302-acc-et-web-pt-a"
-const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+// const COHORT_NAME = "2302-acc-et-web-pt-a"
+// const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
 
-export default function LoginPage({ setToken }) {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [errorMessage, setErrorMessage] = useState("")
-  //const [token, setToken] = useState(localStorage.getItem("token"));
-  const [error, setError] = useState(null)
+function Login({ history }) {
+  const [loading, setLoading] = useState(false);
+  const [alertShow, setAlertShow] = useState(false);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState({
+      email: "",
+      password: ""
+  });
+  const { setUserData } = useContext(Context)
 
-  const navigate = useNavigate()
-  async function submitForm(e) {
-    e.preventDefault()
-    if (password.length < 8) 
-    {
-      setErrorMessage("Password is too short");
-    } else {
-      const signUp = async() => {
-        try {
-          const response = await fetch (`${BASE_URL}/users/register`
-        
-          )}
-        }
-        const login = async () => {
-      try {
-        const response = await fetch(`${BASE_URL}/users/login`, 
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user: {
-              username,
-              password,
-            }
-          })
-        })
-        const { token } = await response.json();
-        localStorage.setItem("token", token);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    login()
+  const handleChanges = (e) => {
+      e.preventDefault();
+      setUser({ ...user, [e.target.name]: e.target.value });
   }
-    return (
-      <div>
-        <NavBar />
-        <h1>Login</h1>
-        <AuthForm buttonText='Login' />
-        <form onSubmit={submitForm}>
-          <label htmlFor="username">Username: </label>
-          <input
-            value={username}
-            type="username"
-            id="username"
-            onChange={(e) => {
-              setErrorMessage("");
-              setUsername(e.target.value);
-            }}
-          />
-          <br></br>
-          <label htmlFor="password">Password: </label>
-          <input
-            value={password}
-            type="password"
-            id="password"
-            onChange={(e) => {
-              setErrorMessage("");
-              setPassword(e.target.value);
-            }}
-          />
-          <p>{errorMessage}</p>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    )
-          }
+
+  const handleSubmitLogin = (e) => {
+      e.preventDefault();
+      setLoading(true);
+      loginUser(user)
+          .then(res => {
+              if (!res.error) {
+                  setUserData(res.user)
+                  history.push('/')
+              } else {
+                  setLoading(false);
+                  setError(res.error.message);
+                  setAlertShow(true);
+              }
+          }).catch(err => console.error('error from login: ', err))
+  }
+
+  return (
+      <>
+          <SimpleSider />
+          <div className="container auth-form">
+              <h1 className="auth-heading">Sign In</h1>
+              <Form className="col-lg-6" onSubmit={handleSubmitLogin}>
+                  {alertShow &&
+                      <Alert variant="danger" onClose={() => setAlertShow(false)} dismissible>
+                          <p>
+                              {error}
+                          </p>
+                      </Alert>
+                  }
+                  <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" name="email" placeholder="Enter email" onChange={handleChanges} required />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" name="password" placeholder="Password" onChange={handleChanges} required />
+                  </Form.Group>
+                  {loading ?
+                      <Button className="col-lg-12 btnAuth" variant="dark" disabled >
+                          Please wait... <Spinner animation="border" />
+                      </Button>
+                      :
+                      <Button variant="dark" className="col-lg-12 btnAuth" type="submit">Sign In</Button>
+                  }
+                  <p className="bottom-msg-paragraph">Don't have an account? <Link to="/auth/register">Sign Up</Link>!</p>
+              </Form>
+          </div>
+      </>
+  )
 }
+
+export default Login;
