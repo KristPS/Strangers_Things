79be53aad5830c54be1a23faa2286f43a8ec5133
KristPS <126523107+KristPS@users.noreply.github.com>
2023-09-01 14:34:55 -0400

    Update SendMessage.jsx

--- src/Components/SendMessage.jsx
+++ src/Components/SendMessage.jsx
@@ -1,28 +1,27 @@
 import { useState } from 'react'
-import { postMessage, fetchPosts } from '../API/index.js'
-
-export default function SendMessage({ post, _id, token, fetchPosts, setShowMessageForm}) {
-    const [message, setMessage] = useState("")
+import { postMessage } from '../API/index.js'
+ export default function SendMessage({ post, _id, token, fetchPosts, setShowMessageForm}) {
+     const [message, setMessage] = useState("")
     async function handleSubmit(e) {
         e.preventDefault()
         const newMessage = {message:{
-            message:message
-        }}
+             message:message
+         }}
         
-        await postMessage(post._id, token, newMessage)
-        await fetchPosts()
-        setShowMessageForm(false)
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="message">Message: </label>
-            <textarea
+         await postMessage(post._id, token, newMessage)
+         await fetchPosts()
+         setShowMessageForm(false)
+  }
+     return (
+         <form onSubmit={handleSubmit}>
+             <label htmlFor="message">Message: </label>
+             <textarea
                 type="text"
-                id="description"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <button className="submit">Submit</button>
-        </form>
-    )
-}
+                 id="description"
+                 value={message}
+                 onChange={(e) => setMessage(e.target.value)}
+             />
+             <button className="submit">Submit</button>
+         </form>
+     )
+ }
