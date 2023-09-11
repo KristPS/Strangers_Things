8c929b22b87cc89b33a9b866f5b52d884a4225ed
KristPS <126523107+KristPS@users.noreply.github.com>
2023-09-10 20:29:59 -0400

    Update AllPostsCard.jsx

--- src/Components/AllPostsCard.jsx
+++ src/Components/AllPostsCard.jsx
@@ -1,38 +1,58 @@
-import { deletePost, updatePost, fetchMyPosts } from "../API";
-import { Link } from 'react-router-dom'
-import UpdatePost from './UpdatePost.jsx'
-import { useState }from 'react'
+import { createNewPost, deletePost, updatePost } from `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
+const COHORT_NAME = '2302-acc-et-web-pt-a';
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+const POSTS = `${BASE_URL}/posts`;
 
-export default function AllPostsCard({ post, token, fetchMyPosts }) {
-    //TODO show messages once send message is functional
-    const { _id, title, description, location, price, willDeliver, active} = post;
-    const [showForm, setShowForm] = useState(false)
-    async function handleClick(_id, token) {
-        await deletePost(_id, token);
-        await fetchMyPosts();
-    }
-    async function handleEditClick() {
-        // await updatePost(post, _id, token);
-        // await fetchMyPosts();
-        setShowForm(true);
-    }
-    return (
-      <div className="allPosts" key={_id}>
-        <h1>{title}</h1>
-        <p>Description: {description}</p>
-        <p>Price: {price} Location: {location} Will Deliver: {willDeliver ? "Yes" : "No"}</p>
-        {/* <p>{messages.map()}</p> */}
-        <p>Active: {active ? "Yes" : "No"}</p>
-        <button className="deleteButton" onClick={() => handleClick(_id, token)}>Delete Post</button>
-        <button className="editPost" onClick={() => handleEditClick()}>Edit Post</button>
-        {showForm && <UpdatePost post={post} token={token} fetchMyPosts={fetchMyPosts} setShowForm={setShowForm}/> }
+export default function AllPostsCard({ post, token, fetchAllPosts }) {
+  const { _id, title, description, price } = post;
+  const [showForm, setShowForm] = useState(false);
 
-
-        {
-           // make button function to show message form below post. 
-           // must be logged in to send message
-        }
-      </div>
-    )
+  async function handleDeletePost(_id, token) {
+    await deletePost(_id, token);
+    await fetchAllPosts();
   }
+
+  async function handleEditPost() {
+    await updatePost(post, _id, token);
+    await fetchAllPosts();
+    setShowForm(true);
+  }
+
+  async function handleCreateNewPost() {
+    const newPost = {
+      title: "Your New Post Title",
+      description: "Your New Post Description",
+      price: 0,
+      location: "Your New Post Location",
+      willDeliver: false,
+    };
+
+    try {
+      await createNewPost(newPost, token);
+      await fetchAllPosts();
+      setShowForm(true);
+    } catch (error) {
+      console.error('Failed to create a new post:', error);
+    }
+  }
+
+  return (
+    <div className="allPosts" key={_id}>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p>{price}</p>
+
+      <button className="createNewPost" onClick={() => handleCreateNewPost()}>Create New Post</button>
+      <Link to={`/posts/${_id}`}>View Post</Link>
+      <button className="editPost" onClick={() => handleEditPost()}>Edit Post</button>
+      <button className="deletePost" onClick={() => handleDeletePost(_id, token)}>Delete Post</button>
+
+      {showForm && <EditPost post={post} token={token} fetchAllPosts={fetchAllPosts} />}
+      </div>
+  );
+}
+
+          
