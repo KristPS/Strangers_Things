10ea06a9ee149c9ea24b36880236e222f553783c
KristPS <126523107+KristPS@users.noreply.github.com>
2023-09-10 21:51:35 -0400

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,34 +1,38 @@
 import { Link } from "react-router-dom";
 
 export default function NavBar({ token, setToken }) {
-    //show different links if logged in or not
-    //implement logout functionality
-    async function handleClick (token, setToken) {
-      await setToken('')
-    }
+  
+  async function handleClick(token, setToken) {
+    await setToken("");
+  }
   return (
-    <div id='navbar'>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/posts">Posts</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
+    <div id="navbar">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/allPosts">Posts</Link>
           </li>
           <li>
-            <Link to="/logout">Logout</Link>
+            <Link to="/Users_Me">Users</Link>
           </li>
-        <button onClick={() => handleClick(token, setToken)}>Login</button>
-        <button onClick={() => handleClick(token, setToken)}>Logout</button>
-      </ul>
-    </nav>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/logOut">Logout</Link>
+          </li>
+          <button onClick={() => handleClick(token, setToken)}>Login</button>
+          <button onClick={() => handleClick(token, setToken)}>Logout</button>
+          <button onClick={() => handleClick(token)}>Delete Post</button>
+          <button onClick={() => handleClick(token)}>Edit Post</button>
+          <button onClick={() => handleClick(token)}>Create New Post</button>
+          <button onClick={() => handleClick(token, setToken)}>Register</button>
+        </ul>
+      </nav>
     </div>
   );
 }
+
